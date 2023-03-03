18ec206a11016b2e2f4f772a000087c5b2d97feb
maych3n <cheny197@newschool.edu>
2023-03-02 22:26:44 -0500

    Clock

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 console.log('Start ticking...')
+setInterval(showTime, 1000);
+function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    am_pm = "AM";
+  
+    if (hour > 12) {
+        hour -= 12;
+        am_pm = "PM";
+    }
+    if (hour == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+  
+
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+  
+    let currentTime = hour + ":" 
+            + min + ":" + sec + am_pm;
+  
+    document.getElementById("clock")
+            .innerHTML = currentTime;
+
+            if ( 19 > hour > 7) {
+                document.getElementById("sun").style.display = "initial"
+                document.getElementById("moon").style.display = "none"
+                 }
+                
+                 if ( 19 < hour < 7) {
+                    document.getElementById("sun").style.display = "none"
+                    document.getElementById("moon").style.display = "initial"
+                     }
+
+
+    
+       
+
+}
+showTime();
